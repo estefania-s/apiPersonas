326002c03d5b9a1f5a6db4e07408de6f6265a739
estefania-s <96440243+estefania-s@users.noreply.github.com>
2021-12-24 12:57:16 -0300

    Delete api directory

--- api/src/main/java/com/api/dao/PersonaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.api.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.model.Persona;
-
-public interface PersonaDAO extends JpaRepository<Persona, Integer>{
-
-}
